7cc419272cf7b9a01671686f8057daefab6effcd
gustavo_sena <gustavo_sena-alves@estudante.sesisenai.org.br>
2026-08-08 12:11:36 -0300

    ADD -  Fiz mais algumas funcionalidades na sidebar

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -1,9 +1,17 @@
+<nav class="site-nav">
+    <button class="sidebar-toggle">
+        <span class="material-symbols-rounded">chevron_left </span>
+    </button>
+</nav>
+
+
+
 <aside class="sidebar">
     <header class="sidebar-header">
         <img src="<?= BASE_PUBLIC ?>/assets/img/Logo-png.png" alt="CodingNepal" class="header-logo" />
         <!-- Sidebar header -->
         <button class="sidebar-toggle">
-            <span class="material-symbols-rounded">chevron_left </span>
+            <span class="material-symbols-rounded"> menu </span>
         </button>
     </header>
 
@@ -13,56 +21,56 @@
             <span class="material-symbols-rounded"> search </span>
             <input type="search" placeholder="Search..." required> 
         </form>
-        
+   
 
         <!-- Menu list -->
         <ul class="menu-list">
             <li class="menu-item">
                 <a href="#" class="menu-link" active>
                     <span class="material-symbols-rounded">dashboard</span>
-                    <span class="menu-labal">Dashboard</span>
+                    <span class="menu-label">Dashboard</span>
                 </a>
             </li>
         
             <li class="menu-item">
                 <a href="#" class="menu-link">
                     <span class="material-symbols-rounded">insert_chart</span>
-                    <span class="menu-labal">Analystic</span>
+                    <span class="menu-label">Analystic</span>
                 </a>
             </li>
         
             <li class="menu-item">
                 <a href="#" class="menu-link">
                     <span class="material-symbols-rounded">notifications</span>
-                    <span class="menu-labal">Notifications</span>
+                    <span class="menu-label">Notifications</span>
                 </a>
             </li>
         
             <li class="menu-item">
                 <a href="#" class="menu-link">
                     <span class="material-symbols-rounded">star</span>
-                    <span class="menu-labal">Favorites</span>
+                    <span class="menu-label">Favorites</span>
                 </a>
             </li>
         
             <li class="menu-item">
                 <a href="#" class="menu-link">
                     <span class="material-symbols-rounded">storefront</span>
-                    <span class="menu-labal">Products</span>
+                    <span class="menu-label">Products</span>
                 </a>
             </li>
         
             <li class="menu-item">
                 <a href="#" class="menu-link">
                     <span class="material-symbols-rounded">group</span>
-                    <span class="menu-labal">Customers</span>
+                    <span class="menu-label">Customers</span>
                 </a>
             </li>
     
             <li class="menu-item">
                 <a href="#" class="menu-link">
                     <span class="material-symbols-rounded">settings</span>
-                    <span class="menu-labal">Settings</span>
+                    <span class="menu-label">Settings</span>
                 </a>
             </li>
         </ul>
@@ -72,7 +80,7 @@
     <div class="sidebar-footer">
         <button class="theme-toggle">
             <div class="theme label">
-                <span class=" theme-icons material-symbols-rounded"> dark_mode </span>
+                <span class=" theme-icon material-symbols-rounded"> dark_mode </span>
                 <span class="theme-text">Dark Mode</span>
             </div>
             <div class="theme-toggle-track">
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const sidebar = document.querySelector('.sidebar');
-    const sidebarToggle = document.querySelector('.sidebar-toggle');
+const sidebar = document.querySelector(".sidebar");
+const sidebarToggleBtn = document.querySelectorAll(".sidebar-toggle");
+const themeToggleBtn = document.querySelector(".theme-toggle");
+const themeIcon = themeToggleBtn.querySelector(".theme-icon");
+const searchForm = document.querySelector(".search-form");
 
-    if (sidebar && sidebarToggle) {
-        sidebarToggle.addEventListener('click', () => {
-            sidebar.classList.toggle('collapsed');
-        });
+
+
+const updateThemeIcon = () => {
+    const isDark = document.body.classList.contains("dark-theme");
+    themeIcon.textContent = sidebar.classList.contains("collapsed")
+    ? (isDark ? "light_mode" : "dark_mode") : "dark_mode";
+}
+//Apply dark thame if saved or system prefers 
+const savedTheme = localStorage.getItem("theme");
+const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const shouldUseDarkTheme = savedTheme === "dark" || (!savedTheme && systemPrefersDark);
+
+document.body.classList.toggle("dark-theme", shouldUseDarkTheme);
+updateThemeIcon();
+
+//Togle sidebar collapse state on button click
+sidebarToggleBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        sidebar.classList.toggle("collapsed");
+        updateThemeIcon();
+    })
+});
+
+searchForm.addEventListener("click", (event) => {
+    if (sidebar.classList.contains("collapsed")) {
+        sidebar.classList.remove("collapsed");
+        searchForm.querySelector("input").focus();
     }
 });
+
+themeToggleBtn.addEventListener("click", () => {
+    const isDark = document.body.classList.toggle("dark-theme");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+    updateThemeIcon();
+})
+
 
